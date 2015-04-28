770911f62f67cda5eaf274de04c214755c7862db
executeinstaller <drimacus182@gmail.com>
2015-04-28 03:30:08 +0300

    hmm... here we have version 2.0

--- js/main.js
+++ js/main.js
@@ -1,245 +1,159 @@
-Parse.initialize("WKJKY5pMblD7RWgKWPJv3w571rynv5BLSw3RrLzv", "u0gGkM5KA2CJAyxGRWg6SVLxINKDPdkMUh7hCHMk");
+(function () {
 
-var callback_progress = 0;
-var repo_count = 0;
-var group_id;
-var group_screen_name;
-var admins = {};
-var owners = [];
+    function beforeStart() {
+        $('#result').empty();
+    }
 
+    function doWork() {
+        beforeStart();
 
-function beforeStart() {
-    $('#result').empty();
-    admins = {};
-}
-
-function doWork(){
-    beforeStart();
-
-    var group_input = $('#group_id').val().trim();
-    var reg0 = /^.*vk\.com\/(\w+)\/?/;
-    if (reg0.test(group_input)) {
-        var arr = group_input.match(reg0);
-        if (arr.length <= 1) {
-            displayError();
-            return;
+        var group_input = $('#group_id').val().trim();
+        var reg0 = /^.*vk\.com\/(\w+)\/?/;
+        if (reg0.test(group_input)) {
+            var arr = group_input.match(reg0);
+            if (arr.length <= 1) {
+                displayError();
+                return;
+            }
+            group_input = arr[1];
         }
-        group_input = arr[1];
-    }
 
-    // One more transformation
+        // One more transformation
 
-    var reg = /^public\d+$/;
+        var reg = /^public\d+$/;
 
-    if (reg.test(group_input)) {
-        group_input = group_input.replace("public", "club");
-    }
-
-    getGroupId(group_input)
-}
-
-function displayError() {
-    $('#result').html('Error');
-}
-
-function getGroupId(group_input) {
-    var script = document.createElement('SCRIPT');
-    script.src = 'https://api.vk.com/method/groups.getById?group_id=' + group_input + '&v=5.26&callback=getGroupIdCallback';
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
-
-function getGroupIdCallback(result) {
-    if ('error' in result) {
-        displayError();
-        return
-    }
-
-    group_id = - (result['response'][0]['id']);
-    group_screen_name = result['response'][0]['screen_name'];
-
-    getRePosts();
-}
-
-function getRePosts() {
-    var script = document.createElement('SCRIPT');
-    script.src = 'https://api.vk.com/method/wall.get?owner_id=' + group_id + '&extended=1&v=5.26&count=100&callback=getRePostsCallback';
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
-
-function getRePostsCallback(result) {
-    repo_progress = 0;
-    callback_progress = 0;
-
-    if ('error' in result) {
-        $('#result').html('Error');
-        return
-    }
-
-    var posts = result['response']['items'];
-
-    var reposts = [];
-    var arrayLength = posts.length;
-    for (var i = 0; i < arrayLength; i++) {
-        var p = posts[i];
-        if (isRepost(p)) {
-            reposts.push(getPostId(p));
+        if (reg.test(group_input)) {
+            group_input = group_input.replace("public", "club");
         }
+
+        getGroupId(group_input)
     }
 
-    var repostsLength = reposts.length;
-
-    if (repostsLength==0) $('#result').append("No reposts found");
-
-    repo_count = repostsLength;
-    for (var i = 0; i < repostsLength; i++) {
-        repo_progress++;
-        getPostById(reposts[i]);
-    }
-}
-
-function getPostById(post_id) {
-    var script = document.createElement('SCRIPT');
-    script.src = 'https://api.vk.com/method/wall.getById?posts=' + post_id + '&extended=1&v=5.25&copy_history_depth=1&callback=getPostByIdCallback';
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
-
-function getPostByIdCallback(result) {
-    if ('error' in result) {
-        $('#result').html('Error');
-        callback_progress++;
-        return;
+    function displayError(err) {
+        var error_msg = err ? err.error_msg : "";
+        $('#result').html('Error: ' + error_msg);
     }
 
-    var post = result['response']['items'][0];
-    getOwners(post);
+    function getGroupId(group_input) {
+        $.getJSON("https://api.vk.com/method/groups.getById?callback=?",
+            {
+                group_id: group_input,
+                v: "5.26"
+            }, function (result) {
+                if ('error' in result) {
+                    displayError(result.error);
+                    return;
+                }
+                var group_id = -(result['response'][0]['id']);
+                getRePosts(group_id);
+            });
+    }
 
-    $('#result').append("<a href='http://vk.com/wall" + getPostId(post) +"'>" + getPostId(post) +"</a><br/>");
+    function getRePosts(group_id) {
+        $.getJSON('https://api.vk.com/method/wall.get?callback=?',
+            {
+                owner_id: group_id,
+                extended: 1,
+                count: 100,
+                v: "5.26"
+            }, function (result) {
+                if ('error' in result) {
+                    displayError(result.error);
+                    return;
+                }
+                var reposts = result['response']['items']
+                    .filter(isRepost);
 
-    var profiles = result['response']['profiles'];
-    var length = profiles.length;
-    for (var i = 0; i<length; i++) {
-        var pr = profiles[i];
-        if (owners.indexOf(parseInt(pr['id'])) < 0) {
-            $('#result').append("<a href='http://vk.com/id" + pr['id'] + "'>" + pr['first_name'] + " " + pr['last_name'] +"</a><br/>");
-            admins[pr['id']] = {first:pr['first_name'], last:pr['last_name']};
+                reposts.map(function (r) {
+                    wallGetThisRepost(r);
+                });
+                if (reposts.length == 0) $('#result').append("No reposts found");
+            });
+    }
+
+//async.until() //todo further use closure for communication between functions
+
+    function wallGetThisRepost(repost) {
+        var orig = repost.copy_history[0];
+        wallGetReposts(orig.from_id, orig.id, 0, 20, function (json) {
+            var i = 0;
+            var rep_objects = json.response.items
+                .map(function (r) {
+                    return {r: r, pos: i++};
+                })
+                .filter(function (o) {
+                    return o.r.from_id == repost.from_id && o.r.id == repost.id;
+                });
+            if (rep_objects.length == 0) {
+                return; //todo do with async until or whilst
+            }
+
+            var r = rep_objects[0].r;
+            var position = rep_objects[0].pos;
+
+            wallGetReposts(orig.from_id, orig.id, position, 1, function (json) {
+                var post = json.response.items[0];
+                $('#result').append("<p>");
+                $('#result').append("<a href='" + genPostUrl(post) + "'>" + genPostUrl(post) + "</a><br/>");
+                var owners = getOwners(post);
+                if (post.comments.list) post.comments.list.map(function (c) {
+                    owners.push(+c.from_id)
+                }); //add ids from comments
+
+                json.response.profiles
+                    .filter(function (pr) {
+                        return !~owners.indexOf(+(pr.id))
+                    })
+                    .map(function (pr) {
+                        $('#result').append("<a href='http://vk.com/id" + pr.id + "'>" + pr.first_name + " " + pr.last_name + "</a><br/>");
+                    });
+                $('#result').append("</p>");
+            });
+        });
+    }
+
+    function wallGetReposts(owner_id, post_id, offset, count, callback) {
+        $.getJSON('https://api.vk.com/method/wall.getReposts?callback=?',
+            {
+                owner_id: owner_id,
+                post_id: post_id,
+                offset: offset,
+                count: count,
+                v: "5.30"
+            }, callback);
+    }
+
+    function genPostUrl(p) {
+        return 'http://vk.com/wall' + p.from_id + '_' + p.id;
+    }
+
+    function getOwners(p) {
+        var owners = [];
+        var reg = /\[id(\d+)\|/g;
+        var full_text = JSON.stringify(p);
+
+        var result;
+        while ((result = reg.exec(full_text))) {
+            owners.push(parseInt(result[1]));
         }
-    }
-    callback_progress++;
-    if (callback_progress >= repo_count) {
-        finishHim()
-    }
-}
 
-function getPostId(p) {
-    return p['owner_id'] + '_' + p['id'];
-}
+        var copy_history = p['copy_history'][0];
 
-function getOwners(p) {
-    owners = [];
-
-    var reg = /\[id(\d+)\|/g;
-
-    var p_text = p['text'];
-    var copy_history_text = p['copy_history'][0]['text'];
-
-    var result;
-    while ( (result = reg.exec(p_text)) ) {
-        owners.push(parseInt(result[1]));
-    }
-
-    while ( (result = reg.exec(copy_history_text)) ) {
-        owners.push(parseInt(result[1]));
-    }
-
-    var copy_history = p['copy_history'][0];
-
-    var from_id_1 = p['from_id'];
-    var from_id_2 = copy_history['from_id'];
-    var owner_id_2 = copy_history['owner_id'];
-    if ('signer_id' in copy_history) {
-        owners.push(copy_history['signer_id']);
-    }
-
-    if (copy_history['post_type'] == 'reply') {
-        //todo additional check needs to be here
-        var reply_id = copy_history['reply_post_id'];
-
-        var post_where_reply_id = owner_id_2 + "_" + reply_id;
-        getReplyById(post_where_reply_id);
-    }
-
-    owners.push(from_id_1);
-    owners.push(from_id_2);
-    owners.push(owner_id_2);
-}
-
-function getLocOwners(p) {
-    var loc_owners = [];
-    var reg = /\[id(\d+)\|/g;
-
-    var p_text = p['text'];
-    var copy_history_text = p['copy_history'][0]['text'];
-
-    var result;
-    while ( (result = reg.exec(p_text)) ) {
-        loc_owners.push(parseInt(result[1]));
-    }
-
-    while ( (result = reg.exec(copy_history_text)) ) {
-        loc_owners.push(parseInt(result[1]));
-    }
-
-    var copy_history = p['copy_history'][0];
-
-    var from_id_1 = p['from_id'];
-    var from_id_2 = copy_history['from_id'];
-    var owner_id_2 = copy_history['owner_id'];
-    if ('signer_id' in copy_history) {
-        loc_owners.push(copy_history['signer_id']);
-    }
-
-    loc_owners.push(from_id_1);
-    loc_owners.push(from_id_2);
-    loc_owners.push(owner_id_2);
-
-    return loc_owners;
-}
-
-
-function getReplyById(reply_id) {
-    var script = document.createElement('SCRIPT');
-    script.src = 'https://api.vk.com/method/wall.getById?posts=' + post_id + '&extended=1&v=5.25&copy_history_depth=1&callback=getReplyCallback';
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
-
-function getReplyCallback(result) {
-    if ('error' in result) {
-        $('#result').html('Error');
-        return;
-    }
-    // append owners
-    var post = result['response']['items'][0];
-    var loc_owners = getLocOwners(post);
-
-    var profiles = result['response']['profiles'];
-    var length = profiles.length;
-    for (var i = 0; i<length; i++) {
-        var pr = profiles[i];
-        if (owners.indexOf(parseInt(pr['id'])) < 0) {
-            owners.push(parseInt(pr['id']));
+        var from_id_1 = p['from_id'];
+        var from_id_2 = copy_history['from_id'];
+        var owner_id_2 = copy_history['owner_id'];
+        if ('signer_id' in copy_history) {
+            owners.push(copy_history['signer_id']);
         }
+        owners.push(from_id_1);
+        owners.push(from_id_2);
+        owners.push(owner_id_2);
+        return owners;
     }
-}
 
+    function isRepost(item) {
+        return 'copy_history' in item && item.copy_history[0].post_type == 'post';
+    }
 
-function isRepost(item) {
-    return 'copy_history' in item;
-}
-
-function finishHim() {
-    var stat = new Parse.Object("Stat");
-    var quoteQuoteText = $('#quote_quote_text');
-    stat.set("group", group);
-    stat.set("admins", admins);
-    stat.save();
-}
+    window.doWork = doWork;
+})();
